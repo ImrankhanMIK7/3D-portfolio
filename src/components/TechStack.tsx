480f2d674a1a9beda1a199b32ff68a1493d74adc
Imran khan <you@example.com>
2026-06-18 12:34:14 +0530

    Fix IntersectionObserver target ID in TechStack.tsx

--- src/components/TechStack.tsx
+++ src/components/TechStack.tsx
@@ -219,7 +219,7 @@ const TechStack = () => {
   }, []);
 
   return (
-    <div className="techstack">
+    <div className="techstack" id="techstack">
       <h2> My Techstack</h2>
 
       {isActive && (
